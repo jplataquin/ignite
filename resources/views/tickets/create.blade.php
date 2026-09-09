e75710a332af9a99bf68d26915f158a63dca68e5
JP Lataquin <jp.lataquin@gmail.com>
2026-09-09 18:13:21 +0800

    fix

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -456,6 +456,15 @@
                 }
             });
         }
+
+        function escapeHtml(text) {
+            return text
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;");
+        }
     });
 </script>
 @endsection
